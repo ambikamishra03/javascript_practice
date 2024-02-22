d86f6391cf1b05ab258761851fde499cc7ec3900
Ambika Mishra <102850517+ambikamishra03@users.noreply.github.com>
2024-02-22 23:12:14 +0530

    updated todo list

--- todo.js
+++ todo.js
@@ -1,1 +1,23 @@
-// todo app 
+// todo app , store todos in local storage
+
+let title = document.getElementById('title')
+let desc = document.getElementById('desc')
+let todo = document.getElementById('todo')
+submit.addEventListener('click',(e) =>{
+    e.preventDefault()
+    localStorage.setItem('todo',JSON.stringify([title.value,desc.value]))
+    console.log(e);
+    todo.innerHTML= `
+    <h1>Add todos</h1>
+    <h4>${title.value}</h4>
+    <h4>${desc.value}</h4>
+    `
+    title.value = ""
+    desc.value  = ""
+})
+
+delet.addEventListener('click',(e) =>{
+    todo.innerHTML= `<h1>Add todos</h1>`
+    e.preventDefault()
+   localStorage.removeItem('todo')
+})
